d28b03e25b5694beae705a669fd6809fd1ad3a76
Matheus De O Zacanini <133885706+Zacanini@users.noreply.github.com>
2024-05-22 21:48:23 -0300

    Upando aula de Pilha , Nos , Fila

---
+++ Materia-No-Pilha-Fila/Fila.cs
@@ -0,0 +1,34 @@
+class Fila<T>
+{
+    public void Adicionar(T valor)
+    {
+        var novoNo = new No<T>
+        {
+            Valor = valor
+        };
+
+        if (final == null)
+            final = inicio = novoNo;
+        else
+        {
+            final.Proximo = novoNo;
+            final = novoNo;
+        }
+    }
+
+    public void Remover()
+    {
+        if (inicio == null)
+            throw new Exception("Fila vazia!");
+
+        inicio = inicio.Proximo;
+
+        if (inicio == null)
+            final = null;
+    }
+
+    public No<T>? Inicio => inicio;
+
+    private No<T>? inicio = null;
+    private No<T>? final = null;
+}
